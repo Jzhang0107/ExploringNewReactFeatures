c8040bd762598f744bedd4bbf35f62b6f80a8bc1
Jason Zhang <zhang98@bu.edu>
2021-01-07 23:52:34 -0500

    Added useState for counter example

--- src/index.js
+++ src/index.js
@@ -2,25 +2,35 @@ import React, { useState }from 'react';
 import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
 
-const App = () =>
+const App = (props) =>
 {
-  const [count, setCount] = useState(10);
+  const [count, setCount] = useState(props.count ? props.count : 0);
 
   const increment = () =>
   {
     setCount(count + 1);
   }
+  const decrement = () =>
+  {
+    setCount(count - 1);
+  }
+  const reset = () =>
+  {
+    setCount(props.count ? props.count : 0);
+  }
 
   return(
     <div>
-      <h1>Regina is cute!</h1>
+      <h1>Regina is cute</h1>
       <p>The current count is {count}</p>
-      <button onClick={increment}>+1 Increment</button>
+      <button onClick={increment}>+1</button>
+      <button onClick={decrement}>-1</button>
+      <button onClick={reset}>Reset</button>
     </div>
   );
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App count={10} />, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
