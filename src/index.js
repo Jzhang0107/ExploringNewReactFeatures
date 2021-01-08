b44dac87f976db13e26262f3f47677e04ea56232
Jason Zhang <zhang98@bu.edu>
2021-01-08 16:37:10 -0500

    Added simple useEffect for persisting array data on refreshes

--- src/index.js
+++ src/index.js
@@ -1,55 +1,60 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
 
 const NoteApp = () =>
 {
-  //const defaultNotes = []
-  //const defaultNoteText = ''
+    const notesData = JSON.parse(localStorage.getItem('notes'));
 
-  const [notes, setNotes] = useState([]);
-  const [noteTitle, setNoteTitle] = useState('');
-  const [noteBody, setNoteBody] = useState('');
+    const [notes, setNotes] = useState(notesData ? notesData : []);
+    const [noteTitle, setNoteTitle] = useState('');
+    const [noteBody, setNoteBody] = useState('');
 
-  const addNotes = (e) =>
-  {
-      e.preventDefault();
+    const addNotes = (e) =>
+    {
+        e.preventDefault();
 
-      setNotes([
-          ...notes, 
-          {
+
+        setNotes([
+            ...notes, 
+            {
             noteTitle: noteTitle,
             noteBody: noteBody
-          }
-      ]);
+            }
+        ]);
 
-      setNoteTitle('');
-      setNoteBody('');
+        setNoteTitle('');
+        setNoteBody('');
+    }
 
-  }
+    const removeNote = (noteToRemove) =>
+    {
+        setNotes(notes.filter((note) => note.noteTitle !== noteToRemove));
+    }
 
-  const removeNote = (noteToRemove) =>
-  {
-      setNotes(notes.filter((note) => note.noteTitle !== noteToRemove));
-  }
-  return(
-    <div>
-        <h1>Lists</h1>
-        {notes.map((note) => (
-            <div key={note.noteTitle}>
-                <h3>{note.noteTitle}</h3>
-                <p>{note.noteBody}</p>
-                <button onClick={() => removeNote(note.noteTitle)}>Remove</button>
-            </div>
-        ))}
-        <form onSubmit={addNotes}>
-            <input placeholder='Note Title' value={noteTitle} onChange={(e) => setNoteTitle(e.target.value)} />
-            <br />
-            <textarea placeholder='Body' value={noteBody} onChange={(e) => setNoteBody(e.target.value)} />
-            <button>Add item</button>
-        </form>
-    </div>
-  )
+    useEffect(() =>
+    {
+        localStorage.setItem('notes', JSON.stringify(notes))
+    })
+
+    return(
+        <div>
+            <h1>Lists</h1>
+            {notes.map((note) => (
+                <div key={note.noteTitle}>
+                    <h3>{note.noteTitle}</h3>
+                    <p>{note.noteBody}</p>
+                    <button onClick={() => removeNote(note.noteTitle)}>Remove</button>
+                </div>
+            ))}
+            <form onSubmit={addNotes}>
+                <input placeholder='Note Title' value={noteTitle} onChange={(e) => setNoteTitle(e.target.value)} />
+                <br />
+                <textarea placeholder='Body' value={noteBody} onChange={(e) => setNoteBody(e.target.value)} />
+                <button>Add item</button>
+            </form>
+        </div>
+    )
 }
 
 ReactDOM.render(<NoteApp />, document.getElementById('root'));
